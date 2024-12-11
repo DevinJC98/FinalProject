fd0f67399db33910a7e9c5d0accb94f76eb73f49
DevinJC98 <devinjc98@hotmail.com>
2024-12-11 02:14:08 -0700

    A lot
    Removed some animations and added others. replaced a lot of my nonsense text, light responsiveness, javascript cycles through images on hover. css edits. Just the whole enchilada really. i forgot to commit for a day or two

--- typeOut.js
+++ typeOut.js
@@ -1,81 +1,62 @@
-document.body.addEventListener("load", printstringbyletter());
-
-
-function printstringbyletter()
+document.getElementById("hogShack").addEventListener("mouseenter", function imgSwap()
 {
-    console.log("qwert");
+   
+    console.log("Swap");
+    let pictures = ["Images/Porkies.jpg", "Images/misc-hogstuff.jpg", "Images/porkSite.jpg"];
+    let picCount = 0;
     
-let text = "2024 was a wild time...";
-let index = 0;
+    let swap = setInterval(function()
+    {
+        picCount++;
 
-let typing = setInterval(function() {
-
-    intro.innerHTML += text.charAt(index);
-    index++;
-    
-    if(index == text.length)
+        if(picCount >= pictures.length )
         {
-            console.log("fading");
-
-            intro.style.animation = "fadeAnimation 1s linear 1s 1 normal forwards";
-            mainPage.style.animation = "mainload 1.2s linear 2.8s 1 normal forwards"
+            picCount -= picCount;
         }
+        
+        hogShack.src = pictures[picCount];
+    
+    }, 2000)
 
-}, 75);
+    document.getElementById("hogShack").addEventListener("mouseleave", function imgSwap()
+    {
+        clearInterval(swap);
+    })
 
-}
+})
+
+
+
 
 function change()
 {
-    document.body.style.animation = "fadeAnimation 1s linear 0s normal forwards"
-    setTimeout(function(){
+           window.location.href = "post1.html";
 
-        window.location.href = "post1.html";
-
-    }, 1000)
 }
 
 function changehome()
 {
-    document.body.style.animation = "fadeAnimation 1s linear 0s normal forwards"
-    setTimeout(function(){
-
+    
         window.location.href = "index.html";
-
-    }, 1000)
-
+   
 }
 
 function change2()
-{
-    document.body.style.animation = "fadeAnimation 1s linear 0s normal forwards"
-    setTimeout(function(){
 
+{  
         window.location.href = "post2.html";
-
-    }, 1000)
 
 }
 
 function change3()
 {
-    document.body.style.animation = "fadeAnimation 1s linear 0s normal forwards"
-    setTimeout(function(){
-
+    
         window.location.href = "post3.html";
-
-    }, 1000)
 
 }
 function change4()
 {
-    document.body.style.animation = "fadeAnimation 1s linear 0s normal forwards"
-    setTimeout(function(){
-
         window.location.href = "post4.html";
-
-    }, 1000)
-
 }
 
 
